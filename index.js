61e2f748f57a7f8060bbfb7206cb25c8263ea64f
vibhor-voicebot <53684148+vibhor-voicebot@users.noreply.github.com>
2021-08-30 04:50:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const server = app.listen(process.env.PORT || 5000, () => {
 });
 
 const io = require('socket.io')(server);
-socket = io();
-socket.on('connection', function(socket){
+
+io.on('connection', function(socket){
   console.log('a user connected');
 });
 
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
-socket.on('connection', function(socket) {
+io.on('connection', function(socket) {
   socket.on('chat message', (text) => {
     console.log('Message: ' + text);
 
